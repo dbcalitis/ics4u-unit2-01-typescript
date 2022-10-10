29f291dfda73c5a41c8d85b954f31f22a42a667d
dbcalitis <daria.bernice.calitis@mths.ca>
2022-10-10 23:06:22 +0000

    updated MrCoxallStack.ts

--- src/MrCoxallStack.ts
+++ src/MrCoxallStack.ts
@@ -8,10 +8,12 @@
 class MrCoxallStack {
   stack: number[] = []
 
+  # pushItem Method - Adds a new item to the stack array.
   pushItem(pushedNumber: number): void {
     this.stack.push(pushedNumber)
   }
 
+  # showStack Method - Displays the stack array.
   showStack(): void {
     console.log(this.stack)
   }
